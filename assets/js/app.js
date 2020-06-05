8bbca64fe631b3c884b327036715e063e0ac62d8
Gjorgji Mitrevski <gorgimitrevski30@gmail.com>
2020-06-05 15:34:53 +0200

    html and css design

---
+++ assets/js/app.js
@@ -0,0 +1,25 @@
+class ContactProfile {
+    firstName=''
+    lastName=''
+
+    displayName() {
+        return `${firstName} ${lastName}`;
+    }
+
+}
+
+const contactsArray = [
+    ContactProfile(
+        // site detali
+    ),
+    ContactProfile(
+        // site detali
+    ),
+    ContactProfile(
+        // site detali
+    ),
+];
+const myProfile = ContactProfile(firstName='Jason', lastName='H.')
+
+
+
