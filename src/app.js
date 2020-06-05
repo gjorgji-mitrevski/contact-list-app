78f9cb564b20edc90fc14f8460f208a8084f96a7
Gjorgji Mitrevski <gorgimitrevski30@gmail.com>
2020-06-05 22:21:41 +0200

    npm and webpack config done

--- src/app.js
+++ src/app.js
@@ -1,7 +1,25 @@
-class clientProfile{
+class ContactProfile {
 
   constructor(){
-    this.firstName =  '';
-    this.lastName = '';
+    this.firstName='';
+    this.lastName='';
   }
+
+  displayName() {
+      return `${firstName} ${lastName}`;
+  }
+
 }
+
+const contactsArray = [
+  ContactProfile(
+      // site detali
+  ),
+  ContactProfile(
+      // site detali
+  ),
+  ContactProfile(
+      // site detali
+  ),
+];
+const myProfile = ContactProfile(firstName='Jason', lastName='H.')
